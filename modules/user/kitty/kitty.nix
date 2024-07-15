b5b48b665010266e997a53d072022a03a7b8782f
SqrtMz <cvesgaduran@gmail.com>
2024-07-15 15:10:23 -0500

    same

--- modules/user/kitty/kitty.nix
+++ modules/user/kitty/kitty.nix
@@ -3,46 +3,46 @@
 {
     programs.kitty = {
         enable = true;
-        shellIntegration.enableZshIntegration = true;
 
         settings = {
             enable_audio_bell = false;
             window_margin_width = 10;
-            background_opacity = 0.6;
         };
 
         extraConfig = ''
+            background_opacity 0.6
+
             # Black
-            color0 #45475A;
-            color8 #585B70;
+            color0 #45475A
+            color8 #585B70
             
             # Red
-            color1 #F38BA8;
-            color9 #F38BA8;
+            color1 #F38BA8
+            color9 #F38BA8
 
             # Green
-            color2  #A6E3A1;
-            color10 #A6E3A1;
+            color2  #A6E3A1
+            color10 #A6E3A1
             
             # Yellow
-            color3  #F9E2AF;
-            color11 #F9E2AF;
+            color3  #F9E2AF
+            color11 #F9E2AF
             
             # Blue
-            color4  #89B4FA;
-            color12 #89B4FA;
+            color4  #89B4FA
+            color12 #89B4FA
 
             # Magenta
-            color5  #F5C2E7;
-            color13 #F5C2E7;
+            color5  #F5C2E7
+            color13 #F5C2E7
 
             # Cyan
-            color6  #94E2D5;
-            color14 #94E2D5;
+            color6  #94E2D5
+            color14 #94E2D5
 
             # White
-            color7  #BAC2DE;
-            color15 #A6ADC8;
+            color7  #BAC2DE
+            color15 #A6ADC8
         '';
 
     };
