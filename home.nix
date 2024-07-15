d5abcdc96bf50e609140231d90e79627be1b2146
SqrtMz <cvesgaduran@gmail.com>
2024-07-15 18:29:15 -0500

    added wlogout and btop packages

--- home.nix
+++ home.nix
@@ -13,6 +13,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    wlogout
+    btop
     # # Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
