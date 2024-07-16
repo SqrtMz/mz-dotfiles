2a4315b887aff7e17cdbe818deeb85a661e0be92
SqrtMz <cvesgaduran@gmail.com>
2024-07-15 20:10:40 -0500

    zsh.nix added .zshenv

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -1,17 +1,25 @@
 {config, lib, pkgs, ...}:
 
 {
-    programs = {
-    zsh = {
-      enableCompletion = true;
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+  programs.zsh = {
+    enableCompletion = true;
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
 
-      oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-      };
+    envExtra = ''
+      QT_STYLE_OVERRIDE = kvantum
+      QT_QPA_PLATFORM = wayland
+      QT_QPA_PLATFORMTHEME = qt6ct
+      XDG_SESSION_TYPE = wayland
+      XCURSOR_SIZE = 20
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = 1
+      QT_AUTO_SCREEN_SCALE_FACTOR = 1
+    '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
     };
   };
 }
