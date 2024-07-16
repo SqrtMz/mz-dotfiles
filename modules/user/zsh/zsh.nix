678bfc1cca84d234de053719d71aad48d37515b5
SqrtMz <cvesgaduran@gmail.com>
2024-07-15 20:12:12 -0500

    fix

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -8,13 +8,13 @@
     syntaxHighlighting.enable = true;
 
     envExtra = ''
-      QT_STYLE_OVERRIDE = kvantum
-      QT_QPA_PLATFORM = wayland
-      QT_QPA_PLATFORMTHEME = qt6ct
-      XDG_SESSION_TYPE = wayland
-      XCURSOR_SIZE = 20
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = 1
-      QT_AUTO_SCREEN_SCALE_FACTOR = 1
+      QT_STYLE_OVERRIDE=kvantum
+      QT_QPA_PLATFORM=wayland
+      QT_QPA_PLATFORMTHEME=qt6ct
+      XDG_SESSION_TYPE=wayland
+      XCURSOR_SIZE=20
+      QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      QT_AUTO_SCREEN_SCALE_FACTOR=1
     '';
 
     oh-my-zsh = {
