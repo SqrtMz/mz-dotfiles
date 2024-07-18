de149da7975e7800a0f241601d8dedd97ef1210d
SqrtMz <cvesgaduran@gmail.com>
2024-07-18 11:25:18 -0500

    changed config to themes

--- modules/user/kvantum/kvantum.nix
+++ modules/user/kvantum/kvantum.nix
@@ -1,8 +1,8 @@
 {config, lib, pkgs, ...}:
 
 {
-    home.file.".config/Kvantum/kvantum.kvconfig" = {
-        source = ./config;
+    home.file.".config/Kvantum/" = {
+        source = ./themes;
         recursive = true;
     };
 }
