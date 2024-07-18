e595bcf72e728ff0d2b30616760109094ad418cd
SqrtMz <cvesgaduran@gmail.com>
2024-07-17 19:16:15 -0500

    test

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/env bash
 
-#stow -t ~/.config -S fastfetch
 #stow -t ~/.local/share -S fonts
-stow -t ~/.config -S hypr
 #stow -t ~/.local/share -S icons
-#stow -t ~/.config -S kitty
 #stow -t ~/.config -S Kvantum
-#stow -t ~/.config -S nvim
 #stow -t ~/.config -S rofi
 #stow -t ~/.config -S sway
 #stow -t ~/.config -S waybar
-#stow -t ~/.config -S wlogout
--- modules/user/kitty/kitty.nix
+++ modules/user/kitty/kitty.nix
@@ -1,4 +1,4 @@
-{config, lib, pkgs, ...}:
+{ config, lib, pkgs, ... }:
 
 {
     home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
