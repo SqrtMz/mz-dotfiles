cca84ad5e98ab7cfa51075a81f94657de1d692cc
SqrtMz <cvesgaduran@gmail.com>
2024-07-18 11:00:31 -0500

    added to home.nix

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   imports = [
     ./modules/user/hyprland/hyprland.nix
     ./modules/user/kitty/kitty.nix
+    ./modules/user/kvantum/kvantum.nix
     ./modules/user/rofi/rofi.nix
     ./modules/user/waybar/waybar.nix
     ./modules/user/wlogout/wlogout.nix
