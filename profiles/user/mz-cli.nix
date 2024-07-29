c3146fe17d54eca0d4987f2edad32762be7f1914
SqrtMz <cvesgaduran@gmail.com>
2024-07-29 12:02:57 -0500

    mz-cli changed

--- profiles/user/mz-cli.nix
+++ profiles/user/mz-cli.nix
@@ -5,9 +5,6 @@
     ../../modules/user/zsh/zsh.nix
   ];
 
-  home.packages = with pkgs; [
-  ];
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   home.stateVersion = "24.05";
