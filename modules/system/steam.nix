872288593dc297b68a05cc903ef45a719af8f6a1
SqrtMz <cvesgaduran@gmail.com>
2024-08-02 21:36:00 -0500

    firewall options for steam.nix

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -3,6 +3,12 @@
 {
     programs.steam = {
         enable = true;
+        remotePlay.openFirewall = true; 
+        dedicatedServer.openFirewall = true;
+
+        extraCompatPackages = with pkgs; [
+            proton-ge-bin
+        ];
 
         gamescopeSession = {
             enable = true;
