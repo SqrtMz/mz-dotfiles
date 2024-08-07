625dde5eb94cd62d9e2ec143ef29a8729b608b32
SqrtMz <cvesgaduran@gmail.com>
2024-08-06 21:25:39 -0500

    opentabledriver to Mz.nix

--- profiles/system/Mz.nix
+++ profiles/system/Mz.nix
@@ -140,6 +140,8 @@
 
         gvfs.enable = true; # Partitions on Dolphin
     };
+    
+    hardware.opentabletdriver.enable = true;
 
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
