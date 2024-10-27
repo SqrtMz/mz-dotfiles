920ee1b9e68e6af7165e64f2affcc1e2e5e7beca
SqrtMz <cvesgaduran@gmail.com>
2024-10-27 17:39:56 -0500

    added neovim.nix with basic config

--- modules/user/neovim/neovim.nix
+++ modules/user/neovim/neovim.nix
@@ -3,11 +3,23 @@
 {
     programs.neovim = {
         enable = true;
-
         defaultEditor = true;
 
-        plugins = with pkgs.vimPlugins; [
-            wtf-nvim
-        ];
+        extraLuaConfig = ''
+            vim.o.tabstop = 4
+            vim.o.shiftwidth = 4
+
+            vim.o.clipboard = "unnamedplus"
+
+            vim.o.signcolumn = "no"
+            vim.o.number = true
+            
+            vim.o.termguicolors = true
+            vim.o.mouse = "a"
+        '';
+
+        # plugins = with pkgs.vimPlugins; [
+        #     gitsigns-nvim
+        # ];
     };
 }
