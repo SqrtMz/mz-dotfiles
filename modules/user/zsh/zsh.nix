4b0fd04f10fba0a773726c7597b952589881d480
SqrtMz <cvesgaduran@gmail.com>
2025-03-08 15:57:15 -0500

    add null zsh package on nix configuration and create pacman package list

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -6,6 +6,7 @@
         enable = true;
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
+		package = pkgs.emptyDirectory;
 
         oh-my-zsh = {
         	enable = true;
