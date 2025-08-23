f5158be2f2aa4bcd81b111d818c8b4420ecdaa22
SqrtMz <cvesgaduran@gmail.com>
2025-08-23 10:07:47 -0500

    Add initial configuration for Satty program

---
+++ modules/user/satty/satty.nix
@@ -0,0 +1,18 @@
+{config, lib, pkgs, inputs, ...}:
+
+{
+    programs.satty = {
+        enable = true;
+        package = (config.lib.nixGL.wrap pkgs.satty);
+
+        settings = {
+            general = {
+                fullscreen = true;
+                copy-command = "wl-copy";
+                actions-on-right-click = [];
+                actions-on-enter = ["save-to-clipboard", "exit"];
+                actions-on-scape = ["exit"];
+            }
+        };
+    };
+}
