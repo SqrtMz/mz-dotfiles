2ccc4c93d27629013271c1e799cb4b99798ed417
SqrtMz <cvesgaduran@gmail.com>
2026-07-21 15:05:10 -0500

    added Mzerver monitor configuration

--- modules/hypr/monitors.lua
+++ modules/hypr/monitors.lua
@@ -1,5 +1,5 @@
 hl.monitor({
-	output = "DP-1",
+	output = "desc:Shenzhen KTC Technology Group H27T6 0000000000000",
 	mode = "2560x1440@210",
 	position = "0x0",
 	scale = 1,
@@ -8,7 +8,7 @@ hl.monitor({
 })
 
 hl.monitor({
-	output = "DP-2",
+	output = "desc:Shenzhen KTC Technology Group H25T7         000000000000",
 	mode = "1920x1080@180",
 	position = "2560x0",
 	scale = 1,
@@ -17,12 +17,17 @@ hl.monitor({
 })
 
 hl.monitor({
-	output = "HDMI-A-1",
-	disabled = true,
+	output = "desc:LG Electronics LG FULL HD 104NTLE9C472",
+	disabled = false,
 	mode = "1920x1080@75",
 	position = "0x0",
 	scale = 1,
 	vrr = 1,
 	cm = "srgb",
 	mirror = "DP-1",
+})
+
+hl.monitor({
+	output = "LVDS-1",
+	disabled = true,
 })
